301ed5f7802a7dd5eca89e47c10bb6a86bbdfae9
Carlos Cote <cotemedinacarlos@iesilipamagna.es>
2019-05-15 13:54:41 +0200

    Copied htmls and turned them into php

---
+++ php/cabecera.php
@@ -0,0 +1,13 @@
+<header>
+	<div>
+		<a href="index.html"><img src="imagenes/GZ-logo.png" alt="Logo" class="logo"></a>
+		<nav>
+			<ul>
+				<li class="ul-item"><a href="bonos.php">Bonos</a></li>
+				<li class="ul-item"><a href="torneos.php">Torneos</a></li>
+				<li class="ul-item"><a href="iniciaSesion.php">Inicia sesión</a></li>
+				<li class="ul-item"><a href="registrate.php">Regístrate</a> </li>
+			</ul>				
+		</nav>	
+	</div>
+</header>
