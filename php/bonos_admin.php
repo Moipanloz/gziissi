75d5a47e8441af863f1169549c5b22d49a0ef070
Carlos Cote <cotemedinacarlos@iesilipamagna.es>
2019-05-19 18:47:43 +0200

    Ivan sql

--- php/bonos_admin.php
+++ php/bonos_admin.php
@@ -36,6 +36,56 @@ $conexion = crearConexionBD();
     <h2 class="titulo">Administración Bonos</h2>
     <div class="admin_class">
         <div class="grid-container-admin-class">
+
+
+        <div id="consumibles-div">
+            <h2>Consumibles</h2>
+            <span>
+
+					<!--FOR EACH CONUSMIBLE-->
+                            <h3>Nombre Consumible</h3>
+
+
+                    <?php $todosLosConsumibles = consultarTodosConsumibles($conexion);
+
+                    foreach ($todosLosConsumibles as $c) {
+
+
+                        if (isset($editandoConsumible) && ($editandoConsumible ["c"] == $c)) { ?>
+
+                            <h3><input type="text" name="CONUSMIBLE" value="<?php print $c ["NOMBRECONSUMIBLE"] ?>"
+                                       id="CONUSMIBLE"/></h3>
+
+
+                            <input id="guardar" name="guardar" type="submit" value="Guardar"
+                                   class="boton_administracion">
+
+
+                        <?php } else { ?>
+
+
+                            <ul>
+							<!-- FOR EACH ELEMENTO -->
+						<li><?php print ($c ["NOMBRECONSUMIBLE"] . " - " . $c["TIPOCONSUMIBLE"]) ?></li>
+                                <!---->
+					</ul>
+
+                            <input id="editar" name="editar" type="submit" value="Editar" class="boton_administracion">
+						<!---->
+						<input id="borrar" name="borrar" type="submit" value="Borrar" class="boton_administracion">
+
+
+                        <?php }
+
+
+                    }
+
+                    ?>
+
+            </span>
+        </div>
+
+
             <!--FOR EACH BONO HASTA MAXIMO DE 3 POR PAG-->
             <div>
                 <!-- CUANDO ESTE EDITANDO -->
