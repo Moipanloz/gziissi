e90cdf804a7918918d9fdf2f70b7a3071ac4c716
Carlos Cote <cotemedinacarlos@iesilipamagna.es>
2019-05-19 13:35:29 +0200

    Adminsitration page now safe (now available for acces)

--- php/administracion.php
+++ php/administracion.php
@@ -11,17 +11,16 @@
         <link rel="icon" type="image/png"  href="imagenes/favicon-32x32.png">
 	</head>
 
-	<!--
 		<?php include_once ("cabecera.php");
 
-		if (!isset($_SESSION ["ADMIN"]))
+		/*if (!isset($_SESSION ["ADMIN"]))
 
             Header("Location: index.php");
 
+		*/
 
 		?>
 
--->
 
 	<body>
 
